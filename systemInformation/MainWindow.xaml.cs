8c0f1e4edb856cf3769edea94f2cc01693d2fb6e
georg rütsche <georg.ruetsche@hotmail.com>
2020-09-24 22:15:50 +0200

    changes

---
+++ systemInformation/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace systemInformation
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string test = "Systeminformation";
+
+        public string Test
+
+        {
+
+            get { return test; }
+
+            set { test = value; }
+
+        }
+
+        public List<String> Items
+        {
+            get { return list; }
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DriveSpace();
+            this.DataContext = this;
+           
+        }
+
+        public List<string> list = new List<string>();
+
+        public void DriveSpace()
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            //List<string> list = new List<string>();
+            foreach (DriveInfo d in allDrives)
+            {
+                list.Add($"Drive { d.Name}");
+                list.Add($"Drive type { d.DriveType}");
+                if (d.IsReady == true)
+
+                    list.Add($"Volume label: { d.VolumeLabel}");
+                    list.Add($"File system: { d.DriveFormat}");
+                    list.Add($"Available space to current user: bytes,{ d.AvailableFreeSpace}");
+
+                   list.Add($"Total available space: bytes, { d.TotalFreeSpace} ");
+                    list.Add($"Total size of drive: bytes { d.TotalSize}");
+                }
+            }
+        }
+    }
+
